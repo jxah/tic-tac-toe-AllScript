840255dc2042fb9a3ec9efe4ae28c0c410e02da1
jxah <jxah@example.com>
2023-07-03 12:47:46 -0400

    Python.py

---
+++ games/Python.py
@@ -0,0 +1,20 @@
+# print the game board
+
+gameboard ='''  
+{0}|{1}|{2}
+---|---|---
+{3}|{4}|{5} 
+---|---|---
+{6}|{7}|{8}
+'''
+
+gameMap = """  
+ 0 | 1 | 2 
+---|---|---
+ 3 | 4 | 5 
+---|---|---
+ 6 | 7 | 8 
+"""
+
+print("Welcome to tic-tac-toe.  Here is the game map:")
+print(gameMap)
