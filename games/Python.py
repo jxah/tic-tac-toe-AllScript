a0bf07c19f68f32d66de29d9066f84f94bba55c5
jxah <jxah@example.com>
2023-11-10 07:34:07 -0500

    Push_for_personalibility

--- games/Python.py
+++ games/Python.py
@@ -15,29 +15,46 @@ gameMap = """
 ---|---|---
  6 | 7 | 8 
 """
-
-print("Welcome to tic-tac-toe.  Here is the placment game map:")
+playerX = input("What is playerX's name? (optional)")
+playerO = input("What is playerO's name? (optional)")
+print("Welcome to tic-tac-toe", playerX,".  Here is the placment game map:")
 print(gameMap)
+done = 0
 
 gameState = ['   ']*9
-#print(gameState)
 #gameState[0] = ' x '
 print(gameboard.format(*tuple(gameState)))
 
-while True: #game play loop
-    invalid = 1
-    while invalid == 1: #check input loop
-        enterx = input("Enter space number for player X:")
+while done == 0: #game play loop
+    invalid = 0
+    enterx = int(input("Enter space number for " + playerX + ":"))
+    print(enterx," entered")
+    if (gameState[enterx] == " X ") or (gameState[enterx] == " O ") : invalid = 1
+    else : invalid = 0
+    while invalid == 1: 
+        print('Sorry, that space is already taken')
+        enterx = int(input("Enter space number for " + playerX + ":"))
         print(enterx," entered")
         if (gameState[enterx] == " X ") or (gameState[enterx] == " O ") : invalid = 1
         else : invalid = 0
     gameState[enterx] = " X "
     print(gameboard.format(*tuple(gameState)))
-    invalid = 1
-    while invalid == 1: #check input loop
-        entero = input("enter space number for player O:")
+    entero = int(input("Enter space number for " + playerO + ":"))
+    if gameState[entero] != "   " : invalid = 1
+    else : invalid = 0
+    while invalid == 1: 
+        print('Sorry, that space is already taken')
+        entero = int(input("Enter space number for " + playerO + ":"))
         if gameState[entero] != "   " : invalid = 1
         else : invalid = 0
     gameState[entero] = " O "
     print(gameboard.format(*tuple(gameState)))
-(done) = 0 
+    #print(gameState)
+done = 0 
+
+while True:
+    #if gameState : ['X'],['X'],['X'],['X' or 'O']*6 or ['X' or 'O']*3,['X'],['X'],['X'],['X' or 'O']*3 or ['X' or 'O']*6,['X'],['X'],['X']:
+    if done == 1:
+        print("X wins")
+    if done == 2:
+        print("O WINS")
